bba73b6fb342289aeceaea86f109cb320eafdbc0
AndreaGirlando <andreagirlando05@gmail.com>
2024-11-01 11:58:09 +0100

    commit

--- Primo anno/programmazione-1/C/scambioNumero.c
+++ Primo anno/programmazione-1/C/scambioNumero.c
@@ -3,22 +3,24 @@
 int main()
 {
     int numero = 0;
+    int reverseNumber(int numero);
 
     printf("Inserisci un numero:");
     scanf("%d",&numero);
 
-    printf("\nNumero con le cifre invertite: ");
 
+    printf("\nNumero con le cifre invertite: %d\n", reverseNumber(numero));
+
+    return 0;
+}
+
+int reverseNumber(int numero){
     int resto = 0;
-
+    int reverse = 0;
     while(numero > 0){
-
         resto = numero % 10;
         numero = numero / 10;
-        printf("%d", resto);
-        // printf("\nResto: %d e quoziente: %.1f",resto, (float)numero / 10);
+        reverse = reverse * 10 + resto;
     }
-
-    printf("\n");
-    return 0;
+    return reverse;
 }
