9cde54ecc3cb57ec37d6be8a894f034a32d318ca
AndreaGirlando <andreagirlando05@gmail.com>
2025-01-14 15:19:30 +0100

    commit

--- Primo anno/programmazione-1/C/Esercizi/EsamePratico/10-01-2023/main.c
+++ Primo anno/programmazione-1/C/Esercizi/EsamePratico/10-01-2023/main.c
@@ -10,6 +10,9 @@ struct param{
 typedef struct param param;
 param decodeParameters();
 char* replaceChar(char s1[256],char s2[256], char c);
+char* invertString(char s3[256]);
+char* sort(char s5[256]);
+void printResult(char s6[256]);
 int main(){
 
     param data = decodeParameters();
@@ -19,9 +22,52 @@ int main(){
     printf("\ndata.s2 = %s", data.s2);
     printf("\ndata.c = %c", data.c);
 
-    printf("\n\nProva: %s",replaceChar(data.s1,data.s2,data.c));
-
+    char *s3 = replaceChar(data.s1,data.s2,data.c);
+    printf("\n\ns3: %s",s3);
+    char *s4 = invertString(s3);
+    printf("\n\ns4: %s",s4);
+    char *s5 = strcat(s3, s4);
+    printf("\n\ns5: %s",s5);
+    char *s6 = sort(s5);
+    printf("\n\ns6: %s",s6);
+    printResult(s6);
     return 0;
+}
+
+void printResult(char s6[256]){
+    printf("\n\n\n\n\n\n");
+    for(int i = 0;i < strlen(s6);i++){
+        if(s6[i]%2 == 1){
+            // printf("*");
+        }else{
+            printf("%c",s6[i]);
+        }
+    }
+    printf("\n");
+}
+
+char* sort(char s5[256]){
+    int len = strlen(s5);
+    char *s6 = malloc(sizeof(char)*len);
+    strcpy(s6,s5);
+    for(int i = 0;i<len;i++){
+        for(int j = 0;j<len-1;j++){
+            if(s6[i]<s6[j]){
+                char temp = s6[i];
+                s6[i] = s6[j];
+                s6[j] = temp;
+            }
+        }
+    }
+    return s6;
+}
+char* invertString(char s3[256]){
+    char *s4 = malloc(sizeof(char)*256);
+    for(int i = 0; i<strlen(s3); i++){
+        s4[i] = s3[strlen(s3)-i-1];
+    }
+    s4[strlen(s3)] = '\0';
+    return s4;
 }
 
 char* replaceChar(char s1[256],char s2[256], char c){
