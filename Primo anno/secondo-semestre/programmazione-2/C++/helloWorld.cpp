7550861adf1b2d4f509d52ee48efb46452962012
AndreaGirlando <andreagirlando05@gmail.com>
2025-03-12 20:45:09 +0100

    2025-03-12 20:45:09

---
+++ Primo anno/secondo-semestre/programmazione-2/C++/helloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!";
+
+    char nome[30];
+
+    cin.getline(nome,30);
+    cout << nome;
+
+    cout << endl;
+    return 0;
+}
