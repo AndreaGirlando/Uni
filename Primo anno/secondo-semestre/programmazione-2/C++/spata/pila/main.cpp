4cabea3354a3776102b9954af605d47ec7b913b0
AndreaGirlando <andreagirlando05@gmail.com>
2025-05-15 16:29:40 +0200

    commit

---
+++ Primo anno/secondo-semestre/programmazione-2/C++/spata/pila/main.cpp
@@ -0,0 +1,56 @@
+/*
+    Scrivere una funzione copia pila che copia il contenuto di una pila in un'altra
+    La funzione deve avere due argomenti di tipo pila, uno per la pila sorgente e un altro per la pila destinazione
+    SPIEGAZIONE: uso una pila d'appoggio per la copia temporanea e poi la uso per riempire di nuovo le due pile
+*/
+
+#include <iostream>
+using namespace std;
+class Nodo{
+    public:
+        Nodo(int _n, Nodo* _next = nullptr): n(_n), next(_next) {}
+        int n;
+        Nodo* next;
+};
+
+class Pila{
+    public:
+        Pila(): head(nullptr){};
+        bool isEmpty(){
+            return head == nullptr;
+        }
+        int getLen(){
+            return len;
+        }
+        void push(Nodo* newNodo){
+            newNodo->next = head;
+            head = newNodo;
+            len++;
+        }
+        Nodo* pop(){
+            if(this->isEmpty()) return nullptr;
+            Nodo* tempHead = head;
+            head = tempHead->next;
+            return tempHead;
+        }
+
+    private:
+        Nodo* head;
+        int len;
+};
+
+void copiaPila(Pila* src, Pila* dst){
+    cout << src->getLen();
+}
+
+int main(){
+
+    Pila temp;
+    temp.push(new Nodo(5));
+    temp.push(new Nodo(8));
+    temp.push(new Nodo(2));
+    temp.push(new Nodo(14));
+    cout << (temp.pop()) -> n;
+    // cout << (temp.pop()) -> n << endl;
+    return 0;
+}
