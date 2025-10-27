c64f858358dc6fb78f893287dbb6a25736ad2a19
AndreaGirlando <andreagirlando05@gmail.com>
2025-10-27 09:37:45 +0100

    2025-10-27 09:37:44

--- 2104 interazioni-multimedia/file-condivisi/mathlab/piani_di_colore.m
+++ 2104 interazioni-multimedia/file-condivisi/mathlab/piani_di_colore.m
@@ -4,13 +4,13 @@ close all
 I=imread('lena.jpg');
 figure,imshow(I);
 
-figure,imshow(I(:,:,1)),title('rosso');
-figure,imshow(I(:,:,2)),title('verde');
-figure,imshow(I(:,:,3)),title('blu');
+% figure,imshow(I(:,:,1)),title('rosso');
+% figure,imshow(I(:,:,2)),title('verde');
+% figure,imshow(I(:,:,3)),title('blu');
 
 figure,mesh(I(:,:,1));
 figure,surf(I(:,:,1));
 return
-
-figure,mesh(I(:,:,2));
-figure,mesh(I(:,:,3));
+% 
+% figure,mesh(I(:,:,2));
+% % figure,mesh(I(:,:,3));
