c8e621618d9ab50eacdc2fcbf035eb326e9ac4a2
AndreaGirlando <andreagirlando05@gmail.com>
2026-03-20 09:41:47 +0100

    2026-03-20 09:41:47

--- 2202 ingegneria-del-software/java/esercizi/main.java
+++ 2202 ingegneria-del-software/java/esercizi/main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        int numero;
+        int numero = 5;
         System.out.println("Prova");
         System.out.println(numero);
     }
